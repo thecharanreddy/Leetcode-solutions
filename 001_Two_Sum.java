ac87e180d27a4754fc3180acd6a1db6898e7e16b
thecharanreddy <charanreddya18@gmail.com>
2025-01-31 22:21:50 +0530

    001_Two_Sum.java

--- 001_Two_Sum.java
+++ 001_Two_Sum.java
@@ -15,4 +15,18 @@ class Solution {
     }
 }
 
-//
+//Optimised method using Hashmap 
+//Time-Complexity : O(n), where n is the size of the array
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int comp = target - nums[i];
+            if(hm.containsKey(comp))    
+                return new int[]{hm.get(comp),i};
+            hm.put(nums[i],i);
+        }
+        return new int[]{};
+    }
+}
