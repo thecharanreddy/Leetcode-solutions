7a1853133bb07b46a0b6875011de3e370bbd801d
thecharanreddy <charanreddya18@gmail.com>
2025-02-02 22:39:46 +0530

    Create 035_Search_Insert_Position.java

---
+++ 035_Search_Insert_Position.java
@@ -0,0 +1,22 @@
+// Binary Search
+// Time complexity : O(log n)
+// Space Complexity : O(1)
+
+class Solution {
+  public int searchInsert(int[] nums, int target) {
+    int l = 0;
+    int r = nums.length;
+
+    while (l < r) {
+      final int m = (l + r) / 2;
+      if (nums[m] == target)
+        return m;
+      if (nums[m] < target)
+        l = m + 1;
+      else
+        r = m;
+    }
+
+    return l;
+  }
+}
