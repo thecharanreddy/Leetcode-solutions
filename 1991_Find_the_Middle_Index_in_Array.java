b907999074d7bf96cfcfcf46aa271dce80f6cfe3
thecharanreddy <charanreddya18@gmail.com>
2025-03-17 13:17:01 +0530

    Create 1991_Find_the_Middle_Index_in_Array.java

---
+++ 1991_Find_the_Middle_Index_in_Array.java
@@ -0,0 +1,56 @@
+// 1991. Find the Middle Index in Array
+// https://leetcode.com/problems/find-the-middle-index-in-array/description/
+
+// Approach : creating leftSum and rightSum arrays, Checking for index with same sum value in arrays.
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+        int[] leftSum = new int[nums.length];
+        int[] rightSum = new int[nums.length];
+
+        int left = 0;
+        for(int i = 0; i < nums.length; i++){
+            leftSum[i] = left;
+            left += nums[i];
+        }
+
+        int right = 0;
+        for(int i = nums.length - 1; i >= 0; i--){
+            rightSum[i] = right;
+            right += nums[i];
+        }
+
+        for(int i = 0; i < nums.length; i++){
+            if(leftSum[i] == rightSum[i])
+                return i;
+        }
+        return -1;
+    }
+}
+
+
+// Approach : Without using extra space 
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int totalSum = 0, leftSum = 0;
+        
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (leftSum == totalSum - leftSum - nums[i]) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        
+        return -1;
+    }
+}
+
